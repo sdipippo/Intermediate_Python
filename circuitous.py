6c0663417574a01ab6577ceadb93eb4b8984af33
Steve Dipippo (sdipippo) <sdipippo@cisco.com>
2017-04-11 10:45:27 -0400

    added version

--- circuitous.py
+++ circuitous.py
@@ -3,16 +3,28 @@
 
 '''
 
+import math
+from collections import namedtuple
+
+Version = namedtuple('Version', 'major minor patch')
+
 class Circle:
     'advanced circle analytics toolkit'
+
+    #major, minor, patch
+    version = Version(0, 1, 0)
 
     def __init__(self, radius):
         self.radius = radius
 
+    def __repr__(self):
+        'Change how the name appears in IDLE'
+        return 'Circle(%r)' % self.radius
+
     def area(self):
         'Quadrature on a planar shape of uniform revolution'
-        return 3.14159 * self.radius ** 2
+        return math.pi * self.radius ** 2
 
     def circumference(self):
         'permeter of a circle'
-        return 3.14 * self.radius * 2
+        return math.pi * self.radius * 2
---
+++ circuitous_client.py
@@ -0,0 +1,10 @@
+'''
+Our customers' code.
+'''
+
+from circuitous import Circle
+
+# Customer 1: Our academic friends.
+
+print 'Proposal to research the areas of circles'
+print 'using Circuitous(tm)', Circle.version
