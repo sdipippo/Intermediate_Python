9fcdaf512281adce095214ec7969d2f938c44737
Steve Dipippo (sdipippo) <sdipippo@cisco.com>
2017-04-12 17:33:04 -0400

    Built two functions into docfinder and tested them

--- docfinder.py
+++ docfinder.py
@@ -35,6 +35,17 @@ from contextlib import closing #helps close connections
 import sqlite3, os, re
 import bz2 #compresses large information, similar to gzip
 
+# These are the public interface of the module
+# If someone imports *, they get the below modules
+# It's also a good indication of what functions should be used
+# by you if you are a user
+__all__ = ['create_db',
+           'add_document',
+           'get_document',
+           'search',
+           'UnknownURI',
+           'DuplicateURI']
+
 class UnknownURI(Exception):
     'uri not found'
 
@@ -42,6 +53,7 @@ class DuplicateURI(Exception):
     'uri already exists'
 
 stopwords = {'the', 'and', 'of', 'is'}
+database = 'pepsearch.db'
 
 def create_db(force=False): # force recreation of database, by default false
     '''
@@ -54,12 +66,29 @@ def normalize(words):
     Transform words into standardized terms by lowercasing,
     de-pluralizing, and ignoring stopwords
     '''
+    normalized_words = []
+    for word in words:
+        lowercased = word.lower()
+        if lowercased not in stopwords:
+            singular = lowercased.rstrip('s')
+            normalized_words.append(singular)
+    return normalized_words
 
-def score_document(text, n=200, pattern=r'[a-z]+'):
+    # lowercased = [word.lower() for word in words]
+    # return [word.rstrip('s') for word in lowercased if word not in stopwords]
+        
+
+def score_document(text, n=200, pattern=r'[A-Za-z]+'):
     '''
     Calculate relevance scores for the 'n'
     most frequent terms in a text
     '''
+    words = re.findall(pattern, text)
+    terms = normalize(words)
+    counts = Counter(terms).most_common(n)
+    # passing something to Counter creates a dictionary of counts
+    total = len(terms)
+    return [(term, count / total) for term, count in counts]
     
 def add_document(uri, text):
     '''
