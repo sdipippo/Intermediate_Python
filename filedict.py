2e621e15e0c2ddba68b9bb5380778e57bcf678f7
Steve Dipippo (sdipippo) <sdipippo@cisco.com>
2017-04-12 12:37:09 -0400

    Completed filesystem dict program using pickle to read and write I commented out the f.write sequences which read like regular text files

--- filedict.py
+++ filedict.py
@@ -10,14 +10,23 @@ a dict-like object backed by the filesystem
 '''
 
 from collections import MutableMapping
-import os
+import os, pickle
 
 class FileDict(MutableMapping):
     'dict-like, backed by file-system'
     #keys are filenames, values are file data
 
-    def __init__(self, folder):
+    def __init__(self, folder, *args, **kwargs):
+        try:
+            os.mkdir(folder)
+        except OSError:
+            pass # don't make a folder, it already exists            
         self.folder = folder
+        self.update(*args, **kwargs)
+
+    def __repr__(self):
+        return '%s(%r, %r)' % (type(self).__name__,
+                                   self.folder, self.items())
 
     def __len__(self):
         len(os.listdir(self.folder))
@@ -26,15 +35,26 @@ class FileDict(MutableMapping):
         #Writes a file (key) and puts data in it (value)
         filepath = os.path.join(self.folder, key)
         with open(filepath, 'w') as f:
-            f.write(value)
-
+            #f.write(repr(value)) #repr represents the tuple as a string
+            pickle.dump(value, f)
+            
     def __getitem__(self, key):
         filepath = os.path.join(self.folder, key)
         with open(filepath) as f:
-            return f.read()
+            return pickle.load(f)
+            #return eval(f.read())
 
-    
-    
+    def __delitem__(self, key):
+        filepath = os.path.join(self.folder, key)
+        os.remove(filepath)
+
+    def __iter__(self):
+        #Iterate through the keys
+        return iter(os.listdir(self.folder))
+        #optional way to do it
+        #for filename in os.listdir(self.folder):
+            #yield filename
+
 
 if __name__ == '__main__':
     d = FileDict('starwars')
